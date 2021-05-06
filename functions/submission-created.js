2213a7592d5435b93577251a9c3f745a924080a7
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-05-06 09:34:27 +0530

    Now we have the integration with Fauna done

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,18 +1,45 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
-    const body = JSON.parse(event.body);
-    console.log(body.payload.data);
+  const body = JSON.parse(event.body);
+  const { email, fullName, interests } = body.payload.data;
 
-    const {email, fullName} = body.payload.data;
+  console.log(email, fullName, interests);
 
-    console.log(email, fullName);
+  const response = await fetch("https://graphql.fauna.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
+    },
+    body: JSON.stringify({
+      query: `
+        mutation($fullName: String!, $email: String!, $interests: [String!]!) {
+            createEntry(data: { fullName: $fullName, email: $email, interests: $interests }) {
+            _id
+            fullName
+            email
+            interests
+          }
+        }      
+      `,
+      variables: {
+        fullName,
+        interests,
+        email,
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
 
-    return {
-        statusCode: 302,
-        headers: {
-          Location: 'success.html',
-          'Cache-Control': 'no-cache',
-        },
-        body: JSON.stringify({})
-      }
+  console.log(response);
 
+  return {
+    statusCode: 302,
+    headers: {
+      Location: "success.html",
+      "Cache-Control": "no-cache",
+    },
+    body: JSON.stringify({}),
+  };
 };
